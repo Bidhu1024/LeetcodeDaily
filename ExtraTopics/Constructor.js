15ab210cfc45222371a9573bab8817227e3880b6
Bidhu1024 <bidhu1024av@gmail.com>
2023-09-07 09:10:03 +0530

    ddddd

---
+++ ExtraTopics/Constructor.js
@@ -0,0 +1,9 @@
+
+function Alien(name,tech){
+this.name=name;
+this.tech=tech
+}
+let alien = new Alien("name", "tech")
+
+
+console.log(alien)
---
+++ practise.js
@@ -0,0 +1,4 @@
+let arr = [1,2,3,4,5,6,7,8]
+
+const a=arr.slice(0,4)
+console.log(a)
